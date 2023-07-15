1cb4a02c189f5b71ad6b99c1bc3ed600ba7a4e85
[Riccardo Uras] <[riccardo.uras3@gmail.com]>
2023-07-15 11:06:51 +0200

    commit

---
+++ database/seeders/ProjectSeeder.php
@@ -0,0 +1,20 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use App\Models\Project;
+use Faker\Generator as Faker;
+class DatabaseSeeder extends Seeder
+{
+    public function run(Faker $faker)
+    {
+        for ($i = 0; $i < 50; $i++) {
+            $project= new Project();
+
+            $project-> title = $faker->sentence(3);
+            $project-> content = $faker->text(500);
+            $project-> image = $faker->imageUrl(800, 600, 'animals', true);
+            $project->save();
+
+        }
+    }
+}
